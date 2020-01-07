3900f707780d61f9986ebe882faf72094e111d8e
lvbin <2632003786@qq.com>
2020-01-08 00:17:56 +0800

    :heavy_minus_sign: remove styled-components

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,14 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-// import "styled-components";
-// declare module "styled-components" {
-//   export interface DefaultTheme {
-//     borderRadius: string;
-
-//     colors: {
-//       primary: string;
-//       secondary: string;
-//     };
-//   }
-// }
