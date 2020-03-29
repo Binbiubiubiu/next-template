91be775fa58d79050a6b6437fe6743fd38ee2283
Simon-Bin <2632003786@qq.com>
2020-03-29 11:08:42 +0800

    feat: 添加tailwinds(适合小场景使用css)

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,17 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: [
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}',
-    // etc.
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => content.match(/[\w-/:]*[\w-/:]/g) || [],
-});
-
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('tailwindcss'),
+    process.env.NODE_ENV === 'production'
+      ? [
+          '@fullhuman/postcss-purgecss',
+          {
+            content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : undefined,
     require('postcss-preset-env')({ stage: 1 }),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-    require('cssnano'),
+    process.env.NODE_ENV === 'production' ? require('cssnano') : undefined,
   ],
 };
